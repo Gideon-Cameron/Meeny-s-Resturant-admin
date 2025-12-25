883decc4058af6c3aa4872ee8f73499e6862ae8c
Gideon Cameron <gideonwork20@gmail.com>
2025-12-25 00:22:07 -0800

    Created the about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // import React from "react";
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
+import About from "./Components/About";
 import Work from "./Components/Work";
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
 
       {/* Hero / About Section */}
       <Hero />
+      <About />
       <Work />
 
       {/*
