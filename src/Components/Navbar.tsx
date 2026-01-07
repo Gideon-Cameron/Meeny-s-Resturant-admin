d1fdfd1e8207774c6f261e759f1f38b2bab93dfe
Gideon Cameron <gideonwork20@gmail.com>
2026-01-06 21:18:33 -0800

    Added the icon to the navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
+import meenysLogo from "../assets/meenys-logo.png";
 
 const Navbar = () => {
   const location = useLocation();
@@ -15,20 +16,18 @@ const Navbar = () => {
     <nav className="w-full bg-white border-b border-gray-200">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="flex h-16 items-center justify-between">
-          
+
           {/* Logo */}
-          <div className="flex items-center">
-            <Link to="/" className="flex items-center gap-2">
-              <img
-                src="/assets/favicon.png"
-                alt="Meeny's Kitchen & Grill logo"
-                className="h-10 w-auto"
-              />
-              <span className="text-lg font-semibold text-gray-900">
-                Admin
-              </span>
-            </Link>
-          </div>
+          <Link to="/" className="flex items-center gap-3">
+            <img
+              src={meenysLogo}
+              alt="Meeny’s Kitchen & Grill logo"
+              className="h-10 w-auto"
+            />
+            <span className="text-lg font-semibold text-gray-900">
+              Admin
+            </span>
+          </Link>
 
           {/* Navigation Links */}
           <div className="flex items-center gap-6">
