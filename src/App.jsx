65f3790ae1ad928cd984581a680b94f3b5b946ae
Gideon Cameron <gideonwork20@gmail.com>
2026-01-07 20:51:52 -0800

    First finished version of the site

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "./Components/Navbar";
 
 import ActiveOrders from "./pages/ActiveOrders";
 import ArchiveOrders from "./pages/ArchiveOrders";
